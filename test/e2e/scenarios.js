335a4770fca551c9eb1a00b9fa4fd65240d8c4d6
David Drayton <draytond@gmail.com>
2014-10-26 16:58:20 -0400

    add search filter with e2e test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,10 +2,25 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('bikeStoreApp', function() {
 
-  beforeEach(function() {
-    browser.get('app/index.html');
-  });
+	describe('overview of all bikes', function(){
+		beforeEach(function() {
+	    	browser.get('app/index.html');
+	  	});
 
+		it('should filter the list of bikes based on the search field input', function(){
+			var bikeList = element.all(by.repeater('bike in bikes'));
+			var search = element(by.model('bikeFilter'));
+
+			expect(bikeList.count()).toBe(5);
+
+			search.sendKeys('best');
+			expect(bikeList.count()).toBe(1);
+
+			search.clear();
+			search.sendKeys('ca');
+			expect(bikeList.count()).toBe(3);
+		});
+	});
 });
