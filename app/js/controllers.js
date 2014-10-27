8d0e192f4c277e3cd6c9ab455af7efcf4cbbc3d0
David Drayton <draytond@gmail.com>
2014-10-27 07:20:58 -0400

    re-adding templates, add details page with css, no tests yet

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,7 @@
 /* Controllers */
 var bikeStoreControllers = angular.module("bikeStoreControllers", []);
 
-bikeStoreControllers.controller("BikesOverviewCtrl", ["$scope", "$http", 
+bikeStoreControllers.controller("BikesOverviewCtrl", ["$scope", "$http",
 	function($scope, $http){
 		$http.get('bikes/allbikes.json').success(function(data) {
 			$scope.bikes = data;
@@ -12,7 +12,10 @@ bikeStoreControllers.controller("BikesOverviewCtrl", ["$scope", "$http",
 	$scope.order = 'name';
 }]);
 
-bikeStoreControllers.controller("bikeDetailCtrl", ["$scope", "$routeParams", 
-	function($scope, $routeParams) {
+bikeStoreControllers.controller("bikeDetailCtrl", ["$scope", "$routeParams", "$http",
+	function($scope, $routeParams, $http) {
+    $http.get('bikes/' + $routeParams.id + '.json').success(function(data) {
+      $scope.bike = data;
+    });
 		$scope.currentId = $routeParams.id;
 }]);
